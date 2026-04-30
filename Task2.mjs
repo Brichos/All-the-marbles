dd2eb091ea37bb62dd2eb81af737976c9e82ed45
Brichos <nicholasak@uia.no>
2026-04-30 21:33:36 +0200

    Added answers to the questions and updated the code to fit question 5

--- Task2.mjs
+++ Task2.mjs
@@ -1,7 +1,20 @@
-const user = { name: "Sara", score: 10};
-const backup = user;
+const user = { name: "Sara", score: 10 };
+const backup = { ...user };
 
 backup.score = 25;
 
 console.log(user.score);
-console.log(backup.score);
+console.log(backup.score); 
+
+/* Answers:
+1. 25 is printed by the two console.log() calls
+
+2. "Backup = user" doesn't copy the object. Both user and backup point to the same.
+
+3. "Backup" is not a copy, it is just the same object with a new name.
+
+4.Changing the object inside a function also changes the original object outside of it.
+
+5. Fixed version is now the one written on top
+*/ 
+
