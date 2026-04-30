13586efac46c06a56037cd8a510bf140f73c0d3e
Brichos <nicholasak@uia.no>
2026-04-30 20:57:41 +0200

    Solved task 1 a

--- Task1.mjs
+++ Task1.mjs
@@ -1,11 +1,21 @@
 //A
 function calcTotal(prices){
     let total = 0;
-    for(let i = 1; i <= prices.length; i++) {
-        total += price[i];
+    for(let i = 0; i < prices.length; i++) {
+        total += prices[i];
     }
-    return total.toFixed(2)
+    return Number (total.toFixed(2));
 };
+
+/* Explanation: 
+1. For loop starts at 1 instead of 0. 
+The total will always miss the first items price because arrays start at 0
+
+2. The loop used i <= prices.length - makes it go 1 too far and then reads undefined
+this makes the result NaN.
+
+3. price[i] needs to be prices[i] because price is not defined
+*/
 
 //B
 
