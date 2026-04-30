527bd5be4ed6dcbd351bd74cb810fe33224d21a2
Brichos <nicholasak@uia.no>
2026-04-30 21:41:05 +0200

    Added answers and updated the code to be correct

--- Task3.mjs
+++ Task3.mjs
@@ -1,9 +1,23 @@
-let points = 5
+let points = 5;
 
-function addPoints(points){
-    points = points + 10;
-    return points;
+function addPoints(value) {
+    return value + 10;
 }
 
-console.log(addPoints(2));
+points = addPoints(2);
+
 console.log(points);
+
+/* Answers:
+1. 12 is printed
+
+2. 5 is printed
+
+3. The "points" variable is a separate one from the one inside the function. 
+Changing it wont change the inner one
+
+4. The variable has the same as the one outside of the function. So the one inside hides the outer one.
+The "let points" variable is invisible form the function.
+
+5. Current code is the rewritten one.
+*/
