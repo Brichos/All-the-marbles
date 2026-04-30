c0f396923931660361b9ca45204e1b976bc1f991
Brichos <nicholasak@uia.no>
2026-04-30 20:43:29 +0200

    First task written in

---
+++ Task1.mjs
@@ -0,0 +1,12 @@
+//A
+function calcTotal(prices){
+    let total = 0;
+    for(let i = 1; i <= prices.length; i++) {
+        total += price[i];
+    }
+    return total.toFixed(2)
+};
+
+//B
+
+//C
